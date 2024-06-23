5ef87813b91f708539b5f3a9508c048c219a09d0
Mustafa <MustafaGino1@yahoo.com>
2024-06-23 09:26:08 -0500

    added a seperate input for each file under pages folderin vite config  in order to get access to the other pages after deployment.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,11 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                project: resolve(__dirname, "/pages/project.html"),
+                project: resolve(__dirname, "pages/project.html"),
+                graphicProject: resolve(__dirname, "pages/graphic-project.html"),
+                marketingProject: resolve(__dirname, "pages/marketing-project.html"),
+                photographyProject: resolve(__dirname, "pages/photography-project.html"),
+                webProject: resolve (__dirname, "pages/web-project.html"),
             },
         },
     },
